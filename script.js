9f12dca4a42958a2101b16154ec1998e80fbbe11
Siclemon <247134392+Siclemon@users.noreply.github.com>
2026-06-23 17:02:19 +0200

    tests

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const nameInput = document.getElementById('nom');
 const namePreview = document.getElementById('name');
 const descInput = document.getElementById('effet');
@@ -12,42 +14,33 @@ descInput.addEventListener("change", () => {
     descPreview.textContent = descInput.value;
 });
 
-const truc = canvas.getContext('2d');
-
-truc.font = '30px sans-serif'
-truc.fillText('Text caca',20,100);
-document.getElementById('canvas').toDataURL('efdchu/jpeg')
-
 // Source - https://stackoverflow.com/a/50300880
 // Posted by Ulf Aslak, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-06-22, License - CC BY-SA 4.0
 
-var download = function(){
-  var link = document.createElement('a');
-  link.download = 'filename.png';
-  link.href = document.getElementById('canvas').toDataURL()
-  link.click();
-}
+// var download = function () {
+//     var link = document.createElement('a');
+//     link.download = 'filename.png';
+//     link.href = document.getElementById('canvas').toDataURL()
+//     link.click();
+// }
 
-function loadFont(name,url){
-  //Loads a custom font from Google Fonts
-  //Make sure that the URL is a path to a valid font file
-  let customFont = new FontFace(name, "url(" + url + ")");
 
-  customFont.load().then((font) => {
-    document.fonts.add(font);
-    console.log("Font loaded: "+name);
-  });
+
+// html2canvas(document.body).then(function (canvass) {
+//     document.body.appendChild(canvass);
+//     canvass.toDataURL('fhel.png')
+// });
+
+function downloadTicket() {
+    const ticketElement = document.getElementById("card");
+
+    html2canvas(ticketElement).then((canvass) => {
+        const link = document.createElement("a");
+        link.download = "conference-ticket.png";
+        link.href = canvass.toDataURL("image/png");
+        link.click();
+    });
 };
 
-loadFont("Animatic", "https://fonts.gstatic.com/s/specialelite/v11/XLYgIZbkc4JPUL5CVArUVL0ntnAOSA.woff2");
-
-function init() {
-  let canvas = document.getElementById("canvas");
-  let ctx = canvas.getContext("2d");
-  ctx.fillStyle = "#0f0";
-  ctx.font = "15pt Animatic";
-  ctx.fillText('text', 10, 10);
-}
-
-init()
+downloadTicket();
